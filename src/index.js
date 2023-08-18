b6e3650aeff14df670b164b795aaccf5bf1a6e17
Carlos Adames <92492690+KamajiRd@users.noreply.github.com>
2023-08-17 20:27:17 -0400

    Setup basic bot

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import {Client } from 'discord.js'
 import { config } from 'dotenv'
+import { REST } from 'discord.js';
 
 config();
 
 const TOKEN = process.env.BOT_TOKEN
 const client = new Client({intents: ['Guilds', 'GuildMessages']})
 
+const rest = REST({ version: '10' }).setToken(TOKEN);
+
 client.login(TOKEN)
 
-client.on('ready', () => {
-    console.log('Bot is online!')
-})
+client.on('ready', () =>  {console.log(`${client.user.tag} has logged in!`)})
 
-client.on('messageCreate', () => {
-    console.log('pon')
-})
