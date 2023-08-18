9f9faafd3347222fd8a9ac79850e8ce4fd28a101
CeeJay-RD <adamescarlos2808@gmail.com>
2023-08-18 12:44:36 -0400

    Added first command "Ban"

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import {Client } from 'discord.js'
+import { Client, GatewayIntentBits } from 'discord.js';
 import { config } from 'dotenv'
 import { REST } from 'discord.js';
 
 config();
 
 const TOKEN = process.env.BOT_TOKEN
-const client = new Client({intents: ['Guilds', 'GuildMessages']})
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const rest = REST({ version: '10' }).setToken(TOKEN);
 
@@ -13,3 +13,19 @@ client.login(TOKEN)
 
 client.on('ready', () =>  {console.log(`${client.user.tag} has logged in!`)})
 
+async function main() {
+    const commands = [
+      BanCommand,     
+    ];
+    try {
+      console.log('Started refreshing application (/) commands.');
+      await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+        body: commands,
+      });
+      client.login(TOKEN);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
+  main();
