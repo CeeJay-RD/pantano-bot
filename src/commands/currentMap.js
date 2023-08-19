ec4f021a7ad6f34f5f58d51868a4f66f197d7562
Carlos Adames <92492690+KamajiRd@users.noreply.github.com>
2023-08-19 15:28:23 -0400

    Added command to get the Apex Legends map

--- src/commands/currentMap.js
+++ src/commands/currentMap.js
@@ -1,7 +1,47 @@
-import { SlashCommandBuilder } from '@discordjs/builders'
+import { SlashCommandBuilder, EmbedBuilder } from '@discordjs/builders'
+import { config } from 'dotenv';
+config();
 
-const currentMap =  new SlashCommandBuilder()
-    .setName('currentmap')
-    .setDescription('Displays information on Apex Legends current map')
+const APEX_TOKEN = process.env.APEX_TOKEN;
 
-export default currentMap.toJSON()
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('map')
+        .setDescription('Returns current Apex Map'),
+
+    async execute(interaction) {
+        const loadingEmbed = new EmbedBuilder()
+            .setDescription('Loading...')
+            //.setColor('(13, 17, 23)'); 
+
+        await interaction.reply({ embeds: [loadingEmbed] }); // Use reply instead of editReply
+
+        try {
+            const response = await fetch(`https://api.mozambiquehe.re/maprotation?auth=${APEX_TOKEN}`);
+            const data = await response.json(); // Added await here
+            const map = data.current.map;
+            const image = data.current.asset
+            const remainingTime = data.current.remainingTimer
+            console.log(map)
+            const mapEmbed = new EmbedBuilder()
+                .setTitle(`Lo tigre se tan tirando en ${map}`)
+                .setDescription('Cuidate que lo tigre andan sweaty y desacatao')
+                .setImage(`${image}`)
+                .setFooter({ text: `Le quedan ${remainingTime} al maldito mapa` });
+
+            await interaction.editReply({ embeds: [mapEmbed] });
+        } catch (error) {
+            console.error(error);
+
+            const errorEmbed = new EmbedBuilder()
+                .setTitle('Error')
+                .setDescription('An error occurred while fetching map data.')
+
+            await interaction.editReply({ embeds: [errorEmbed] });
+        }
+    }
+};
+
+
+
